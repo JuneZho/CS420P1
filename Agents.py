8ee82128afd00923944ebadd0c8a666b7c62e55a
JuneZho <38892180+JuneZho@users.noreply.github.com>
2018-09-10 07:08:24 +0800

    Update Agents.py

--- Agents.py
+++ Agents.py
@@ -1,10 +1,12 @@
 from game import Agent
 from game import Directions
 
+
 class DumbAgent(Agent):
-    "An agent that goes West until it can't."
+    """An agent that goes West until it can't."""
+
     def getAction(self, state):
-        "The agent receives a GameState (defined in pacman.py)."
+        """The agent receives a GameState (defined in pacman.py)."""
         print("Location:", state.getPacmanPosition())
         print("Action available:", state.getLegalPacmanActions())
         if Directions.WEST in state.getLegalPacmanActions():
@@ -14,31 +16,35 @@ class DumbAgent(Agent):
             print("Stopping.")
             return Directions.STOP
 
+
 class RandomAgent(Agent):
-    "Based on the current options, pick a random action to carry out."
+    """Based on the current options, pick a random action to carry out."""
+
     def getAction(self, state):
-         "The agent receives a GameState (defined in pacman.py)."
-         print("Location:", state.getPacmanPosition())
-         print("Action available:", state.getLegalPacmanActions())
-         import random
-         return random.choice(state.getLegalPacmanActions())
+        "The agent receives a GameState (defined in pacman.py)."
+        print("Location:", state.getPacmanPosition())
+        print("Action available:", state.getLegalPacmanActions())
+        import random
+        return random.choice(state.getLegalPacmanActions())
 
 
 class BetterRandomAgent(Agent):
-    "Based on the current options, pick a random action to carry out."
+    """Based on the current options, pick a random action to carry out."""
+
     def getAction(self, state):
-         "The agent receives a GameState (defined in pacman.py)."
-         print("Location:", state.getPacmanPosition())
-         print("Action available:", state.getLegalPacmanActions())
-         actions = state.getLegalPacmanActions()
-         actions.remove(Directions.STOP)
-         import random
-         return random.choice(actions)
+        """The agent receives a GameState (defined in pacman.py)."""
+        print("Location:", state.getPacmanPosition())
+        print("Action available:", state.getLegalPacmanActions())
+        actions = state.getLegalPacmanActions()
+        actions.remove(Directions.STOP)
+        import random
+        return random.choice(actions)
 
 
 class ReflexAgent(Agent):
-    "If one of these actions would cause a food pellet to be eaten, it should choose that action. "
-    "If none of the immediate actions lead to food, it should choose randomly from the possibilities (excluding 'Stop')."
+    """If one of these actions would cause a food pellet to be eaten, it should choose that action.
+    If none of the immediate actions lead to food, it should choose randomly from the possibilities (excluding 'Stop')."""
+
     def getAction(self, state):
 
         legalActions = state.getLegalPacmanActions()
@@ -52,9 +58,6 @@ class ReflexAgent(Agent):
         for score, action in successors:
             if score == highestScore:
                 bestActions.append(action)
-                
+
         import random
         return random.choice(bestActions)
-
-               
-
